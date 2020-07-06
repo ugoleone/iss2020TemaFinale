468a0902dad54c3500e69841280cf57211940b7d
Nicolo Romandini <nicolo.romandini@studio.unibo.it>
2020-07-06 18:43:38 +0200

    refactoring workspace sprint 1

--- Workspace/Sprint1/it.unibo.trc19.sprint1.waiter/waiter.pl
+++ Workspace/Sprint1/it.unibo.trc19.sprint1.waiter/waiter.pl
@@ -2,15 +2,11 @@
 % waiter description   
 %====================================================================================
 context(ctxwaiter, "localhost",  "TCP", "8010").
-context(ctxtable1, "127.0.0.1",  "TCP", "8020").
-context(ctxtable2, "127.0.0.1",  "TCP", "8030").
 context(ctxsmartbell, "127.0.0.1",  "TCP", "8040").
 context(ctxbarman, "127.0.0.1",  "TCP", "8050").
 context(ctxclient, "127.0.0.1",  "TCP", "8060").
 context(ctxbasicrobot, "127.0.0.1",  "TCP", "8070").
- qactor( table1, ctxtable1, "external").
-  qactor( table2, ctxtable2, "external").
-  qactor( smartbell, ctxsmartbell, "external").
+ qactor( smartbell, ctxsmartbell, "external").
   qactor( barman, ctxbarman, "external").
   qactor( client, ctxclient, "external").
   qactor( basicrobot, ctxbasicrobot, "external").
