a43632742503bdb30046c5f407b9f9b1934fe8c0
Nicolo Romandini <nicolo.romandini@studio.unibo.it>
2020-07-11 15:35:31 +0200

    rimossa directory client

--- Workspace/Sprint1/it.unibo.trc19.sprint1.client/client.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% client description   
-%====================================================================================
-context(ctxwaiter, "127.0.0.1",  "TCP", "8010").
-context(ctxsmartbell, "127.0.0.1",  "TCP", "8040").
-context(ctxclient, "localhost",  "TCP", "8060").
- qactor( smartbell, ctxsmartbell, "external").
-  qactor( waiter, ctxwaiter, "external").
-  qactor( client, ctxclient, "it.unibo.client.Client").
