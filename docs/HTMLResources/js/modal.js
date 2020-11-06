d120055e8d196e015ffb1244670f9ef969906bb9
ugoleone <ugoleone.cavalcanti@gmail.com>
2020-11-06 20:01:30 +0100

    webProj update

--- docs/HTMLResources/js/modal.js
+++ docs/HTMLResources/js/modal.js
@@ -1,20 +1,25 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-              
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-//var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  //captionText.innerHTML = this.alt;
-}
+/*
+ * modalID = id del div del modal
+ * imgID = id dell'immagine da usare
+ * modalImgID = id dell'immagine che sta nel modal
+ * closerID = id dell'icona da premere per chiudere il modal
+ */
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+function setModal(modalID, imgID, modalImgID, closerID) {
+  var modal = document.getElementById(modalID);
+  var img = document.getElementById(imgID);
+  var modalImg = document.getElementById(modalImgID);
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    //captionText.innerHTML = this.alt; //qualora si volesse una didascalia
+  }
+
+  //Elemento <span> da premere per chiudere il modale
+  var span = document.getElementById(closerID);
+  
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
 }
